a370c9ceaa12e46a2dfaeea3b89661f439e6c2b5
Olakunle Hassan <31227288+kaylezy@users.noreply.github.com>
2022-06-24 10:36:05 +0100

    simple js calculator

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let num1 = parseInt(prompt("Number1: "));
+let opr = prompt("Operator: +, -, *, /");
+let num2 = parseInt(prompt("Number2: "));
+
+if (opr == "+") {
+  alert(num1 + num2);
+} else if (opr == "-") {
+  alert(num1 - num2);
+} else if (opr == "*") {
+  alert(num1 * num2);
+} else if (opr == "/") {
+  alert(num1 / num2);
+}
+
+
